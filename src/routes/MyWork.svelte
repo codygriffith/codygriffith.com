b4608c96b3ece85b906ccdd5b98fb66b92611f06
codygriffith <cody@alenthea.com>
2022-08-27 11:57:01 -0400

    Update MyWork.svelte

--- src/routes/MyWork.svelte
+++ src/routes/MyWork.svelte
@@ -129,11 +129,11 @@
 						<!-- content here -->
 						<div class="relative">
 							<div class="mobile_render md:mt-[3.5%] md:ml-[6vw]">
-								<iframe class="md:h-[26.75vw] md:w-[14.75vw] mt-[9vw] h-[63.5vw] w-[34.9vw] mr-[-5.75vw] md:mt-[2.2vw] md:mr-[5.45vw]" src={item.url} style="-webkit-transform:scale(0.59);-moz-transform-scale(0.59);" />
+								<iframe title="Mobile version of {item.url}" class="md:h-[26.75vw] md:w-[14.75vw] mt-[9vw] h-[63.5vw] w-[34.9vw] mr-[-5.75vw] md:mt-[2.2vw] md:mr-[5.45vw]" src={item.url} style="-webkit-transform:scale(0.59);-moz-transform-scale(0.59);" />
 								<img src="phone.png" class="w-[23vw] mt-[18vw] md:w-[9.65vw] md:mr-[8vw] md:mt-[6vw]" alt="" />
 							</div>
 							<div class="desktop_render md:mr-[15%] mt-0">
-								<iframe class="md:mt-[-10vw] md:mr-[-11vw] md:w-[64.75vw] md:h-[36.5vw] mt-[-12vw] mr-[-21vw] h-[71.5vw] w-[125vw]" src={item.url} style="-webkit-transform:scale(0.5);-moz-transform-scale(0.5);" />
+								<iframe title="Desktop version of {item.url}" class="md:mt-[-10vw] md:mr-[-11vw] md:w-[64.75vw] md:h-[36.5vw] mt-[-12vw] mr-[-21vw] h-[71.5vw] w-[125vw]" src={item.url} style="-webkit-transform:scale(0.5);-moz-transform-scale(0.5);" />
 								<img src="laptop-png-grey.png" class="h-[60vw] mt-[7vw] lg:mt-[unset] md:mt-0 md:h-[30.8vw]" alt="" />
 							</div>
 						</div>
