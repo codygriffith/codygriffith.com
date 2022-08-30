5ea1a5005198a5286823684cf77367542a6cbb19
codygriffith <cody@alenthea.com>
2022-08-30 17:44:21 -0400

    Update MyWork.svelte

--- src/lib/MyWork.svelte
+++ src/lib/MyWork.svelte
@@ -85,7 +85,7 @@
 									{/if}
 									{#if item.tags.includes("AWS")}
 										<a href="https://aws.amazon.com/" class="hover:transform scale-120 mr-4 mt-2" target="_blank">
-											<svg height="2.25rem" width="2.25rem" viewBox="0 0 301 180" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+											<svg height="2.25rem" width="2.25rem" viewBox="0 0 301 180" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
 												<g transform="matrix(1,0,0,1,-1.66798,-1.1)">
 													<g>
 														<path
